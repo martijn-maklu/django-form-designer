378d6dfc315140d0bcdbcc2856646e46924217cd
Martijn Boerwinkel <martijn.boerwinkel@maklu.nl>
2015-09-04 15:35:05 +0200

    Update models.py

--- form_designer/models.py
+++ form_designer/models.py
@@ -16,7 +16,7 @@ try:
 except ImportError:  # django < 1.5
     from django.contrib.auth.models import User
 else:
-    User = get_user_model()
+    User = settings.AUTH_USER_MODEL
 
 from form_designer.fields import TemplateTextField, TemplateCharField, ModelNameField, RegexpExpressionField
 from form_designer.utils import get_class
